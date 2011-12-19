3f42ccb80513d0eb1262ca2be61d994e03ed0e4a
Scott Nietfeld <snietfeld@gmail.com>
2011-12-18 16:55:39 -0800

    Major update. Begun implementing Babelbits session management stuff. Also begun implementing telemetry fields, subscription tracking, etc. ALL THIS NEEDS TO BE TESTED. Still need to implement packing/unpacking of telemetry packets.

--- babelbits.c
+++ babelbits.c
@@ -26,27 +26,226 @@
 #include <stdio.h>
 #endif
 
+#include <stdint.h>  //Needed for explicit-size datatypes (uint8_t, etc)
+#include <string.h>  //Needed for calculating string sizes
+
 #include "babelbits.h"
 
-
-void processMessage(char* p_msg, unsigned int msgLen)
+void bb_init(bbInstance* p_bb, uint8_t* p_buf, uint16_t buflen)
 {
-  int i;
+  uint8_t i = 0;
+  bbField* p_field;
+
+  p_bb->nfields = 0; //Reset number of registered fields to zero
+
+  p_bb->p_out_msgbuf = p_buf;
+  p_bb->out_msgbuf_len = buflen;
+  p_bb->out_bytes_waiting = 0;
+
+  //Clear all fields & subscriptions
+  for(i = 0; i < BB_MAX_NFIELDS; i++)
+    {
+      p_field = &(p_bb->fields[i]);  //Clear field
+      p_field->p_var = 0;
+      p_field->type = UNKNOWN;
+      p_field->name[0] = 0;  //Null the string
+
+      p_bb->subscriptions[i] = 0;    //Clear subscription
+    }
+}
+
+
+void bb_processMessage(bbInstance* p_bb, uint8_t* p_msg, uint16_t msgLen)
+{
+  uint8_t i;
+  uint8_t msgType;
+  uint8_t* p_innerMsg;
+  uint16_t innerMsgLen;
 
   printf("Message received: \"");
   for (i = 0; i < msgLen; ++i)
     printf("%c", p_msg[i]);
   printf("\"\n");
+ 
+  //Arrange inner message and inner message length
+  msgType = p_msg[0];
+  p_innerMsg = &(p_msg[1]);
+  innerMsgLen = msgLen - 1;
+
+  switch(msgType)
+    {
+    case BB_HANDSHAKE_INIT:
+      p_bb->connection_state = BB_CONNECTED;
+      break;
+
+    case BB_ACK:
+      if(p_bb->connection_state == BB_HANDSHAKE_SENT)
+	p_bb->connection_state = BB_CONNECTED;
+      break;
+
+    case BB_REQUEST_AVAILABLE_FIELDS:
+      break;
+
+    case BB_PUBLISH:
+      printf("Publish message received.\n");
+      break;
+
+    case BB_SUBSCRIBE:
+      bb_process_subscribe(p_bb, (bbMsg_Subscribe*)p_innerMsg);
+      break;
+
+    case BB_UNSUBSCRIBE:
+      bb_process_unsubscribe(p_bb, (bbMsg_Unsubscribe*)p_innerMsg);
+      break;
+
+    case BB_TMPACKET:
+      bb_process_tmpacket(p_bb, (bbMsg_TM*)p_innerMsg);
+      break;
+
+    case BB_TXTPACKET:
+      p_bb->txt_msg_handler(p_innerMsg);
+      break;
+
+    default:
+      printf("Error: unknown packet type\n");
+    }
+
 }
+
+void bb_process_tmpacket(bbInstance* p_bb, bbMsg_TM* p_msg)
+{
+}
+
+void bb_process_subscribe(bbInstance* p_bb, bbMsg_Subscribe* p_msg)
+{
+  p_bb->subscriptions[p_msg->field_id] = 1;
+}
+
+void bb_process_unsubscribe(bbInstance* p_bb, bbMsg_Unsubscribe* p_msg)
+{
+  p_bb->subscriptions[p_msg->field_id] = 0;
+}
+
+
+void bb_register_field(bbInstance* p_bb, void* p_var, enum Datatype type, char* p_name)
+{
+  uint8_t field_id;
+  bbField* p_field;
+
+  p_field = &(p_bb->fields[p_bb->nfields]);
+
+  p_field->p_var = p_var;
+  p_field->type = type;
+  strcpy(p_field->name, p_name);
+
+  p_bb->nfields += 1;
+}
+
+void bb_send_handshake(bbInstance* p_bb)
+{
+  uint8_t* p_buf;
+
+  p_buf = p_bb->p_out_msgbuf;
+
+  *p_buf = BB_HANDSHAKE_INIT;
+
+  p_bb->out_bytes_waiting = 1;
+  p_bb->connection_state = BB_HANDSHAKE_SENT;
+}
+
+int8_t bb_publish_field(bbInstance* p_bb, bbField* p_field)
+{
+  uint8_t i;
+  uint8_t* p_buf;
+  uint8_t* p_buf_end;
+  uint8_t* p_cur;
+
+  uint16_t nbytes;
+
+  //If no outgoing buffer has been set, raise error
+  if(p_bb->p_out_msgbuf == 0) return -1;
+
+  if(p_bb->out_bytes_waiting > 0)
+    printf("bb_publish_field(): Error, out_bytes_waiting = %d", 
+	   p_bb->out_bytes_waiting);
+
+  p_buf = p_bb->p_out_msgbuf;
+  p_cur = p_buf;
+
+  *p_cur = BB_PUBLISH;             //Add BB message type
+  p_cur += 1;
+  
+  //Add field struct to outgoing buff
+  memcpy(p_cur, p_field, sizeof(bbField));
+
+  nbytes = 1 + sizeof(*p_field);
+  printf("\nnbytes: %d\n", nbytes);
+
+  p_bb->out_bytes_waiting = nbytes;
+  return nbytes;
+}
+
+
+
+  /* for(i = 0; i < p_bb->nfields; i++) */
+  /*   { */
+  /*     p_field = &(p_bb->fields[i]); */
+
+  /*     //Calculate number of bytes we'll be adding so we know if we're about to overrun */
+  /*     nbytes = sizeof(bbField) + strlen(p_field->p_name); */
+  /*     if(p_cur + nbytes >= p_buf_end) return -2;  //About to overrun--raise error */
+
+  /*     //Good to go, send field to buffer */
+  /*   } */
+
+
+
+
 
 #ifdef UNIT_TEST
 
+void print_fields(bbInstance* p_bb)
+{
+  int i;
+  bbField* p_field;
 
+  printf("\nRegistered fields:\n");
+  for(i = 0; i < p_bb->nfields; i++)
+    {
+      p_field = &(p_bb->fields[i]);
+	printf("p_var: %p \t type: %d \t name: %s\n", 
+	      p_field->p_var, p_field->type, p_field->name);
+    }
+}
+
+
+bbInstance bbits;
+uint8_t bbuf[64];
 
 void main()
 {
+  uint32_t counter = 0;
+  float pi = 3.141592;
+  
 
+  printf("Initializing babelbits instance...\n");
+  bb_init(&bbits, bbuf, 64);
+  printf("Babelbits instance initialized.\n");
 
+  printf("Registering fields...\n");
+  bb_register_field(&bbits, &counter, INT32, "counter");
+  bb_register_field(&bbits, &pi, FLOAT32, "pi");
+  printf("Fields registered.\n");
+
+  print_fields(&bbits);
+
+  bb_publish_field(&bbits, &(bbits.fields[0]));
+  printf("Done.\n");
+
+  if(bbits.out_bytes_waiting > 0)
+    printf("Bytes waiting to be sent: %d\n", bbits.out_bytes_waiting);
+
+  bb_processMessage(&bbits, bbits.p_out_msgbuf, bbits.out_bytes_waiting);
 }
 
 #endif
--- babelbits.h
+++ babelbits.h
@@ -8,33 +8,141 @@
  * Test configuration: See babelbits.c
  *--------------------------------------------------------------------*/
 
-#define MAX_VARNAMELEN 30
-#define MAX_NUMTMVARS 64
+#include <stdarg.h>   //Needed for variadic function def in bbCommand
+
+#define BB_MAX_VARNAMELEN 32
+#define BB_MAX_NFIELDS 64
+#define BB_MAX_NARGS 16
+#define BB_MIN_BUFLEN 64   //Minimum BB out buffer length needed
+
 
 enum Datatype
   {
-    SHORT,
-    INT,
-    FLOAT,
-    DOUBLE,
-    CHAR
+    UNKNOWN,
+    INT8,
+    INT16,
+    INT32,
+    INT64,
+    UINT8,
+    UINT16,
+    UINT32,
+    UINT64,
+    FLOAT32,
+    FLOAT64,
   };
 
-enum MsgType
+enum bbMsgType
   {
-    HANDSHAKE_INIT,
-    ACK,
-    TMPACKET
+    BB_HANDSHAKE_INIT,                //Initiates communication
+    BB_ACK,                           //Confirms communication
+    BB_REQUEST_AVAILABLE_FIELDS,      //Client asks what fields we have
+    BB_PUBLISH,                       //Host sends list of fields
+    BB_SUBSCRIBE,                     //Client subscribes to a field
+    BB_UNSUBSCRIBE,                   //Client unsubscribes to a field
+    BB_TMPACKET,                      //Host sends telemetry update
+    BB_TXTPACKET,
   };
 
+//Message structs
+typedef struct
+{
+  uint8_t field_id;
+} bbMsg_Subscribe;
+
+typedef struct
+{
+  uint8_t field_id;
+} bbMsg_Unsubscribe;
+
+//---------------
+
+enum bbConnectionState
+  {
+    BB_NO_CONNECTION,
+    BB_HANDSHAKE_SENT,
+    BB_CONNECTED
+  };
+
+enum Unit
+  {
+    NONE,
+    SECONDS, MINUTES, HOURS, DAYS, YEARS,
+    MILLIMETERS, CENTIMETERS, METERS, KILOMETERS, 
+    METERS_PER_SEC, KILOMETERS_PER_HOUR,
+    METERS_PER_SEC_SQUARED,
+    DEGREES_C,
+    GRAMS, KILOGRAMS,
+    NEWTONS,
+    MILLIVOLTS, VOLTS,
+    AMPS
+  };
+
+//Basic TM field - generic housekeeping data
+#pragma pack(push, 1)
 typedef struct
 {
   void* p_var;
   enum Datatype type;
-  char varname[MAX_VARNAMELEN];
-} TMVar;
+  char name[BB_MAX_VARNAMELEN];
+} bbField;
+#pragma pack(pop)
+
+/*
+//Sensor field - has units, can be calibrated
+typedef struct
+{
+  bbField* p_field;
+  enum Unit units;
+} bbSensor;
+
+//Actuator field - has associated sensor, current command
+typedef struct
+{
+  bbSensor* p_sensor;   //Actuator status
+  void* p_cmd;          //Current command
+} bbActuator;
+
+//http://publications.gbdirect.co.uk/c_book/chapter9/stdarg.html
+//http://en.wikipedia.org/wiki/Variadic_function
+typedef struct
+{
+  char name[MAX_VARNAMELEN];   //Could just be a pointer to a user-deffed string?
+  uint8_t nargs;
+  enum Datatype[MAX_NARGS];
+  char argNames[MAX_NARGS][MAX_VARNAMELEN]; //Could just be a desc. string?
+  void (*p_cmdfcn)(...);   //FIXMEOHGOD
+} bbCommand;
+
+*/
+
+//Struct to keep all babelbits info
+typedef struct
+{
+  uint8_t nfields;       //Number of fields registered
+  bbField fields[BB_MAX_NFIELDS]; //Declared w/ max to avoid dynamic mem alloc
+
+  //Array contains bools indicating which fields client is subscribed to
+//Note: could be string of bits instead of bytes & just use bit shifts+mask
+  uint8_t subscriptions[BB_MAX_NFIELDS];
+
+  uint8_t* p_out_msgbuf;       //Buffer for outgoing BB messages
+  uint16_t out_msgbuf_len;     
+  uint16_t out_bytes_waiting;  //Lets user know BB wants to send a msg
+
+  uint8_t connection_state;
+
+  void (*txt_msg_handler)(uint8_t* p_txt);
+} bbInstance;
 
 
-TMVar tmvars[MAX_NUMTMVARS];
+//Public functions
+//------------------------------------------------------------------
+void bb_init(bbInstance* p_bb, uint8_t* p_buf, uint16_t buflen);
+void bb_send_handshake(bbInstance* p_bb);
+void bb_processMessage(bbInstance* p_bb, uint8_t* p_msg, uint16_t msgLen);
+void bb_register_field(bbInstance* p_bb, void* p_var, enum Datatype type, char* p_name);
+int8_t bb_publish_field(bbInstance* p_bb, bbField* p_field);
 
-void processMessage(char* p_msg, unsigned int msgLen);
+//Incoming message handlers
+void bb_process_subscribe(bbInstance* p_bb, bbMsg_Subscribe* p_msg);
+void bb_process_unsubscribe(bbInstance* p_bb, bbMsg_Unsubscribe* p_msg);
